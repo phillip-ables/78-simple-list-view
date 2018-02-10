1f9dacc88bce23884cc8aad2cdcdf9f71b6b6fbf
Phillip <phillipables1@gmail.com>
2018-02-10 13:57:22 -0500

    simple String array data source for the days of the week

--- app/src/main/java/com/example/techtron/simplelistview/MainActivity.java
+++ app/src/main/java/com/example/techtron/simplelistview/MainActivity.java
@@ -8,11 +8,15 @@ import android.widget.ListView;
 public class MainActivity extends Activity {
     //create a simple reference, remember the method findViewById always helps you bring a view from xml to java
     ListView l;
+    //data source
+    String[] days={"Sunday", "Monday", "Tuesday", "Wenesday", "Thursday", "Friday", "Saturday", "Sunday"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //youll probably get an error blah blah blah incopatable types, so you simply alt enter change to android widget listview and that does the automatic typecast for you
         l = (ListView) findViewById(R.id.listView);
+
+
     }
 }
