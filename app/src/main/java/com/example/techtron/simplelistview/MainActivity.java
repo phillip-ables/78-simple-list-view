221bad4e68d107c05baeda79be3f617617ff32ab
Phillip <phillipables1@gmail.com>
2018-02-10 13:55:03 -0500

    simple listview reference that we typecast to the id of listView

--- app/src/main/java/com/example/techtron/simplelistview/MainActivity.java
+++ app/src/main/java/com/example/techtron/simplelistview/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.techtron.simplelistview;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    //create a simple reference, remember the method findViewById always helps you bring a view from xml to java
+    ListView l;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //youll probably get an error blah blah blah incopatable types, so you simply alt enter change to android widget listview and that does the automatic typecast for you
+        l = (ListView) findViewById(R.id.listView);
     }
 }
