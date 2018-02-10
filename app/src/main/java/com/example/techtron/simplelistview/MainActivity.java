7156d3deb7642439f5167262879ae8526bc38987
Phillip <phillipables1@gmail.com>
2018-02-10 14:06:11 -0500

    new ArrayAdapter with this context and simple text layout1 with datat source days

--- app/src/main/java/com/example/techtron/simplelistview/MainActivity.java
+++ app/src/main/java/com/example/techtron/simplelistview/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.techtron.simplelistview;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
@@ -16,7 +17,11 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         //youll probably get an error blah blah blah incopatable types, so you simply alt enter change to android widget listview and that does the automatic typecast for you
         l = (ListView) findViewById(R.id.listView);
-
+        //the list view is made up of two parts one part takes data one part displays the data, the adapter is responsible for taking the data and put it in a single view
+        //array adapter that reads a type string, three parameters that you have to give to your adapter
+        //context, defines how a single row should appear in your list view, data
+        //android has many premade layouts defined and they are in android.R.layout.<define>
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 , days);
 
     }
 }
